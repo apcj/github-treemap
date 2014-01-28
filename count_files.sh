4594b82ea68fbf654fcb966df1939e0facf7d218
Alistair Jones <alistair.jones@gmail.com>
2014-01-28 09:40:02 +0000

    Properly handle counts for nested folders.

--- count_files.sh
+++ count_files.sh
@@ -4,10 +4,10 @@ echo -e "path\tfiles\tchanges"
 
 for i in `find $1 -type d`;
 do 
-   count=`find $i -name "*.java" -or -name "*.scala" -maxdepth 1 -type f|wc -l`
+   count=`find $i -name "*.java" -or -name "*.scala" -type f|wc -l`
    if [ $count -gt 0 ]
      then     
-       changes=`(cd $i; git diff $2 --name-only -- $i)|wc -l`
+       changes=`(cd $i; git diff $2 --name-only -- $i)|grep -e java -e scala|wc -l`
        echo -e $i"\t"$count"\t"$changes
    fi;
 done|cut -b 2-
